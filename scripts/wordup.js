6dbf169fa4805d012a546877c614347e3276a737
Perigree <holisticspiral@gmail.com>
2017-04-03 11:59:38 -0400

    Update wordup.js

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -171,6 +171,7 @@ function render() {
     if (gameOver) {
         // TODO 9
         // disable the text box and clear its contents
+        $("#textbox").attr("disabled", true).val (" ")
 
     }
 }
